ff30b9b3198e6b57b464b47585aef807072f46d1
SeungChul <jeonseungchul@kakao.com>
2026-07-21 02:05:59 +0900

    feat(waiting): 대기 화면 추가
    - 루트(/) 요청에 index 템플릿을 렌더링하는 HomeController
    - thymeleaf 템플릿과 정적 스타일시트 추가

---
+++ waiting-server/src/main/java/com/urban6/waiting/presentation/HomeController.java
@@ -0,0 +1,13 @@
+package com.urban6.waiting.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
